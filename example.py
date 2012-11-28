232afbf15fe51c0fedce2c8d4dcf66fb3ed5bb7e
ericumolu <reach.eric@hotmail.com>
2012-11-28 11:07:09 -0800

    remove xml module

---
+++ example.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3.3
+
+import os
+from measure import Measure,globe
+
+
+globe = Measure()
+
+globe.add_block("main")
+
+globe.add_block("sub1",['group1'])
+
+globe.add_block("sub2")
+for i in range(9999):i
+globe.stop_block()
+
+globe.stop_block()
+
+globe.add_block("sub3",['group1'])
+
+globe.add_block("sub4")
+for i in range(9999):i
+globe.stop_block()
+
+globe.add_block("sub5")
+for c in range(999920):c
+globe.stop_block()
+
+globe.stop_block()
+
+globe.stop_block()
+
+
+print (globe.report())
+
+print (globe.report_group(['group1']))
+
